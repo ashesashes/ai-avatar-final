9808d2c4d419c28cd4dee274e4429a75625d9112
ashesashes <ashi.mbamail@gmail.com>
2023-01-14 02:05:08 -0800

    reverteg ga

--- pages/index.js
+++ pages/index.js
@@ -138,15 +138,6 @@ const Home = () => {
   <Head>
     <title> Jungkook Magic Mirror Portraits</title>
   </Head>
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HCYNLDX2FS"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HCYNLDX2FS');
-</script>
   <div className = "container">
     <div className="header">
       <div className="header-title">
